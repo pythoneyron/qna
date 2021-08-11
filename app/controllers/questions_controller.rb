24cd17c38c934515ab916d477668ebe9ed1cf4b1
Платонов Олег <pythoneyron@gmail.com>
2021-08-11 23:04:59 +0500

    added tests for questions methods create, update, destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,4 @@
 class QuestionsController < ApplicationController
-  before_action :set_questions, only: [:show, :edit]
-
   def index
     @questions = Question.all
   end
@@ -9,15 +7,42 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
   end
 
   def edit
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    if question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    question.destroy
+    redirect_to questions_path
+  end
+
   private
 
-  def set_questions
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  end
+
+  helper_method :question
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
