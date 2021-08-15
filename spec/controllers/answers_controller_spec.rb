6833056ee7f1799b0d5ea7d11591dd4913211240
Платонов Олег <pythoneyron@gmail.com>
2021-08-15 19:40:48 +0500

    Changed test in POST method for answers Answer to question.answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'save a new answers in the database' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirect to show view' do
