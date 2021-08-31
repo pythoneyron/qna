1eadf0e6f4c020567f022cc0a37766fc4aee80b8
Олег Платонов <pythoneyron@gmail.com>
2021-08-31 16:53:44 +0500

    Feature attaching many files

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -49,6 +49,6 @@ class QuestionsController < ApplicationController
   helper_method :question, :answer
 
   def question_params
-    params.require(:question).permit(:title, :body, :file)
+    params.require(:question).permit(:title, :body, files: [])
   end
 end
